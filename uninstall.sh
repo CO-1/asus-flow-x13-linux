7a37bd24515a9ff3ef03c960fbe0a4c606284513
battajehub <67742224+battajehub@users.noreply.github.com>
2021-06-03 20:46:03 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,6 @@
 
 rm /etc/modprobe.d/asus-flow-x13-nouveau.conf
 rm /etc/udev/rules.d/99-asus-flow-power.rules
-dkms remove hid-asus-flow-x13/1.0 --all
+dkms remove asus-flow-x13/1.0 --all
 
 echo "Please rebuild your initramfs and reboot"
